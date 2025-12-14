4af6f647c5ab0aece78bf6e0c7a8d3ff8933a7b2
notlbock <uyenla1224@gmail.com>
2025-12-14 13:51:40 -0800

    Implement engine loader for module scripts
    Added engine loader functionality to load scripts in order.

--- engine.js
+++ engine.js
@@ -1,1 +1,7 @@
-
+// ======== Engine Loader ========
+// This file just loads the modules in order
+Game.loadScript('core.js', () => {
+    Game.loadScript('draw.js', () => {
+        Game.loadScript('scriptLoader.js');
+    });
+});
